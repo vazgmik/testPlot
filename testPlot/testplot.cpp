5d45f1d5e48bd8110d07a073d9040e06a8e1b627
vazgmik <vazgvazg2@gmail.com>
2014-09-16 21:15:53 +0400

    solve bug

--- testPlot/testplot.cpp
+++ testPlot/testplot.cpp
@@ -1,17 +1,11 @@
 #include "testplot.h"
-#include "mythread.h"
+
 
 testPlot::testPlot(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
-	pThread = new MyThread(this);
-	pThread->start();
 	connect(ui.pbStart,SIGNAL(clicked()),SLOT(on_bStart_clicked()));
-//	setupRealtimeDataDemo(ui.plot_wid1);
-	setWindowTitle("QCustomPlot: "+demoName);
-	statusBar()->clearMessage();
-	ui.plot_wid1->replot();
 }
 
 testPlot::~testPlot()
@@ -21,31 +15,10 @@ testPlot::~testPlot()
 
 void testPlot::on_bStart_clicked()
 {
-	/*QVector<double> x(101), y(101); // initialize with entries 0..100
-	for (int i=0; i<101; ++i)
-	{
-		x[i] = i/50.0 - 1; // x goes from -1 to 1
-		y[i] = x[i]*x[i]; // let's plot a quadratic function
-	}
-	// create graph and assign data to it:
-	ui.plot_wid1->addGraph();
-	ui.plot_wid1->graph(0)->setData(x, y);
-	// give the axes some labels:
-	ui.plot_wid1->xAxis->setLabel("x");
-	ui.plot_wid1->yAxis->setLabel("y");
-	ui.plot_wid2->xAxis->setLabel("x");
-	ui.plot_wid2->yAxis->setLabel("y");
-	// set axes ranges, so we see all data:
-	ui.plot_wid1->xAxis->setRange(-1, 1);
-	ui.plot_wid1->yAxis->setRange(0, 1);
-	ui.plot_wid1->replot();
-	ui.plot_wid2->replot();
-	
 	setupRealtimeDataDemo(ui.plot_wid1);
 	setWindowTitle("QCustomPlot: "+demoName);
 	statusBar()->clearMessage();
-
-	ui.plot_wid1->replot();*/
+	ui.plot_wid1->replot();
 }
 
 
@@ -94,7 +67,7 @@ void testPlot::setupRealtimeDataDemo(QCustomPlot *customPlot)
   
   // setup a timer that repeatedly calls MainWindow::realtimeDataSlot:
   connect(&dataTimer, SIGNAL(timeout()), this, SLOT(realtimeDataSlot()));
-  dataTimer.start(0); // Interval 0 means to refresh as fast as possible
+  dataTimer.start(1); // Interval 0 means to refresh as fast as possible
 }
 
 
